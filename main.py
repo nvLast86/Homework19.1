a103610e6cd67c9e03416fd8df9c84130e4d1de7
nvLast86 <n.lastenko@gmail.com>
2023-05-01 22:36:57 +0600

    main.py with hometask has been added

---
+++ main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        c_len = int(self.headers.get("Content-Length"))
+        body = self.rfile.read(c_len)
+        print(body.decode())
+        self.send_response(200)
+        self.send_header("Content_type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes('[]', "utf-8"))
+
+    def do_GET(self):
+        get_answer = 'Hello, World wide web!'
+        self.send_response(200)
+        self.send_header("Content_type", "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(get_answer, "utf-8"))
+
+
+if __name__ == '__main__':
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
